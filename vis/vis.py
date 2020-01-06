80434dbf519fcea9564c6b83c51a7ebe315e00ca
ej550 <edward.oughton@gmail.com>
2020-01-06 19:43:35 +0000

    Add contextily basemap

--- vis/vis.py
+++ vis/vis.py
@@ -11,8 +11,9 @@ import configparser
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# import contextily as ctx
+import contextily as ctx
 import geopandas as gpd
+import matplotlib.colors
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read(os.path.join(os.path.dirname(__file__), '..','scripts', 'script_config.ini'))
@@ -20,31 +21,37 @@ BASE_PATH = CONFIG['file_locations']['base_path']
 
 DATA_RAW = os.path.join(BASE_PATH, 'raw')
 DATA_PROCESSED = os.path.join(BASE_PATH, 'processed')
+DATA_RESULTS = os.path.join(BASE_PATH, '..', 'results')
 
-def create_scatterplot():
+def create_scatterplot(coefficients):
+
+    coeff_urban = round(coefficients['urban'].values[0], 3)
+    coeff_rural = round(coefficients['rural'].values[0], 3)
 
     clust_averages = pd.read_csv(os.path.join(DATA_PROCESSED,
         'lsms-cluster-2016.csv'))
 
     to_plot = clust_averages[["nightlights", "cons", "urban"]]
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,8))
     g = sns.scatterplot(x="nightlights", y="cons", hue="urban", data=to_plot, ax=ax1)
-    g.set(xlabel='Luminosity (DN)', ylabel='Household Consumption ($ per day)',
+    g.set(xlabel='Luminosity (DN)', ylabel='Household Consumption ($ per month)',
         title='Consumption \n vs Luminosity')
 
     handles, labels = ax1.get_legend_handles_labels()
-    ax1.legend(handles=reversed(handles[1:3]), labels=reversed(labels[1:3]))
+    ax1.legend(title='Settlement Type', handles=reversed(handles[1:3]),
+        labels=['Rural ({})'.format(coeff_rural), 'Urban ({})'.format(coeff_urban)])
 
     to_plot = clust_averages[["cons_pred", "cons", "urban"]]
 
     g = sns.scatterplot(x="cons_pred", y="cons", hue="urban", data=to_plot, ax=ax2)
-    g.set(xlabel='Predicted Consumption ($ per day)',
-        ylabel='Household Consumption ($ per day)',
+    g.set(xlabel='Predicted Consumption ($ per month)',
+        ylabel='Household Consumption ($ per month)',
         title='Consumption \n vs Predicted Consumption')
 
     handles, labels = ax2.get_legend_handles_labels()
-    ax2.legend(handles=reversed(handles[1:3]), labels=reversed(labels[1:3]))
+    ax2.legend(title='Settlement Type', handles=reversed(handles[1:3]),
+        labels=['Rural ({})'.format(coeff_rural), 'Urban ({})'.format(coeff_urban)])
 
     fig.tight_layout()
     fig.savefig(os.path.join(BASE_PATH, '..', 'vis','figures','scatterplot.png'))
@@ -52,35 +59,38 @@ def create_scatterplot():
     return print('Completed scatterplot')
 
 
-def create_regplot():
+def create_regplot(coefficients):
+
+    coeff_urban = round(coefficients['urban'].values[0], 3)
+    coeff_rural = round(coefficients['rural'].values[0], 3)
 
     clust_averages = pd.read_csv(os.path.join(DATA_PROCESSED,
         'lsms-cluster-2016.csv'))
 
     to_plot = clust_averages[["nightlights", "cons", "cons_pred", "urban"]]
 
-    urban = to_plot.loc[to_plot['urban'] == 'Urban']
-    rural = to_plot.loc[to_plot['urban'] == 'Rural']
+    urban = to_plot.loc[to_plot['urban'] == 'urban']
+    rural = to_plot.loc[to_plot['urban'] == 'rural']
 
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12,8))
 
     g = sns.regplot(x="nightlights", y="cons", data=urban, ax=ax1)
     g.set(xlabel='Luminosity (DN)', ylabel='Urban HH Consumption ($ per day)',
-        title='Urban Consumption \n vs Luminosity')
+        title='Urban Consumption ({})\n vs Luminosity'.format(coeff_urban))
 
     g = sns.regplot(x="nightlights", y="cons", data=rural, ax=ax2)
     g.set(xlabel='Luminosity (DN)', ylabel='Rural HH Consumption ($ per day)',
-        title='Rural Consumption \n vs Luminosity')
+        title='Rural Consumption ({})\n vs Luminosity'.format(coeff_rural))
 
     g = sns.regplot(x="cons_pred", y="cons", data=urban, ax=ax3)
     g.set(xlabel='Predicted Consumption ($ per day)',
         ylabel='Urban HH Consumption ($ per day)',
-        title='Urban Consumption \n vs Predicted Consumption')
+        title='Urban Consumption ({})\n vs Predicted Consumption'.format(coeff_urban))
 
     g = sns.regplot(x="cons_pred", y="cons", data=rural, ax=ax4)
     g.set(xlabel='Predicted Consumption ($ per day)',
         ylabel='Rural HH Consumption ($ per day)',
-        title='Rural Consumption \n vs Predicted Consumption')
+        title='Rural Consumption ({})\n vs Predicted Consumption'.format(coeff_rural))
 
     fig.tight_layout()
     fig.savefig(os.path.join(BASE_PATH, '..', 'vis','figures','regplot.png'))
@@ -88,8 +98,7 @@ def create_regplot():
     return print('Completed regplot')
 
 
-def plot_map(metric, legend_label, title, hour, roads, flow_min, flow_max, sites,
-    output_filename, metric_min, metric_max):
+def plot_map(data, title, legend_label):
 
     fig, ax = plt.subplots(figsize=(8, 10))
 
@@ -97,36 +106,32 @@ def plot_map(metric, legend_label, title, hour, roads, flow_min, flow_max, sites
     plt.autoscale(tight=True)
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-    # plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
-    #         hspace = -5, wspace = 0)
 
-    roads.plot(
-        # figsize=(8, 10),
-        column=metric,
-        cmap='RdYlBu',#'hot',#
-        norm=matplotlib.colors.Normalize(vmin=-150, vmax=150),
+    data.plot(
+        column='pred_consu',
+        cmap='RdYlBu',
+        norm=matplotlib.colors.Normalize(vmin=0, vmax=200),
         legend=True,
         legend_kwds={'label': legend_label, 'orientation': 'horizontal'},
+        alpha=0.3,
         ax=ax
     )
 
-    sites.plot(
-        column = 'Cell Site',
-        markersize=10,
-        legend=True,
-        ax=ax
-        )
-
-    # plt.legend(, bbox_transform=ax.transAxes)
-    plt.title('{:02d}:00 {}'.format(hour, title), fontsize=16)
-    ctx.add_basemap(ax, crs=roads.crs)
-    plt.savefig(output_filename, pad_inches=0, bbox_inches='tight')
+    plt.title(title, fontsize=16)
+    ctx.add_basemap(ax, crs=data.crs)
+    plt.savefig(os.path.join(BASE_PATH, '..', 'vis','figures','context.png'), pad_inches=0, bbox_inches='tight')
     plt.close()
 
-    return print('Completed {:02d}.png'.format(hour))
+    return print('Completed')
 
 if __name__ == '__main__':
 
-    create_scatterplot()
+    coefficients = pd.read_csv(os.path.join(DATA_RESULTS, 'coefficients.csv'))
 
-    create_regplot()
+    create_scatterplot(coefficients)
+
+    create_regplot(coefficients)
+
+    results = gpd.read_file(os.path.join(DATA_RESULTS, 'results.shp'))
+
+    plot_map(results, 'Predicted Monthy Consumption ($)', 'Monthy Consumption ($)')
