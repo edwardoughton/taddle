43863a85dc91453d2f37e7c85c1fbfd79e4f4f5e
ej550 <edward.oughton@gmail.com>
2020-01-15 16:53:55 +0000

    Capture population per grid square

--- scripts/grid.py
+++ scripts/grid.py
@@ -10,9 +10,11 @@ import argparse
 import os
 import configparser
 import geopandas as gpd
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, mapping
 import pandas as pd
 import numpy as np
+import rasterio
+from rasterstats import zonal_stats
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read('script_config.ini')
@@ -22,16 +24,18 @@ SHAPEFILE_DIR = f'data/{COUNTRY}/shapefile'
 GRID_DIR = f'data/{COUNTRY}/grid'
 IMAGE_DIR = f'data/{COUNTRY}/images'
 
+
 def create_folders():
     os.makedirs(GRID_DIR, exist_ok=True)
+
 
 def generate_grid(country):
 
     filename = 'national_outline_{}.shp'.format(country)
     country_outline = gpd.read_file(os.path.join(SHAPEFILE_DIR, filename))
 
-    country_outline.crs = {'init':'epsg:4326'}
-    country_outline = country_outline.to_crs({'init':'epsg:3857'})
+    country_outline.crs = "epsg:4326"
+    country_outline = country_outline.to_crs("epsg:3857")
 
     xmin,ymin,xmax,ymax = country_outline.total_bounds
 
@@ -47,14 +51,29 @@ def generate_grid(country):
         for y in rows:
             polygons.append( Polygon([(x,y), (x+wide, y), (x+wide, y-length), (x, y-length)]))
 
-    grid = gpd.GeoDataFrame({'geometry':polygons})
+    grid = gpd.GeoDataFrame({'geometry': polygons})
     intersection = gpd.overlay(grid, country_outline, how='intersection')
-    intersection.crs = {'init' :'epsg:3857'}
-    intersection = intersection.to_crs({'init': 'epsg:4326'})
+    intersection.crs = "epsg:3857"
+    intersection = intersection.to_crs("epsg:4326")
 
-    intersection.to_file(os.path.join(GRID_DIR, 'grid.shp'))
+    final_grid = query_settlement_layer(intersection, country)
+
+    final_grid = final_grid[final_grid.geometry.notnull()]
+    final_grid.to_file(os.path.join(GRID_DIR, 'grid.shp'))
 
     print('Completed grid generation process')
+
+
+def query_settlement_layer(grid, country):
+
+    path = os.path.join(SHAPEFILE_DIR, 'MWI.tif')
+
+    grid['population'] = pd.DataFrame(
+        zonal_stats(vectors=grid['geometry'], raster=path, stats='sum'))['sum']
+
+    grid = grid.replace([np.inf, -np.inf], np.nan)
+
+    return grid
 
 
 if __name__ == '__main__':
