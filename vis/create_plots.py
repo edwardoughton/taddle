bc3d86c89cb5698d31ec54604cc0bfb79ee6a4f7
ej550 <edward.oughton@gmail.com>
2020-02-21 18:06:10 +0000

    Format/add doc string api info

--- vis/create_plots.py
+++ vis/create_plots.py
@@ -1,12 +1,13 @@
 """
 Create plots of grid-level predictions across a country
 
-Written by Jatin Mathur
+Written by Jatin Mathur and Ed Oughton
 
 Winter 2020
 
 """
-
+import os
+import sys
 import configparser
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,8 +16,7 @@ import geopandas as gpd
 from shapely.geometry.point import Point
 import matplotlib.cm as cm
 import matplotlib.colors as colors
-import os
-import sys
+import contextily as ctx
 sys.path.append('.')
 from utils import merge_on_lat_lon
 
@@ -29,18 +29,40 @@ CONFIG.read('script_config.ini')
 COUNTRY = CONFIG['DEFAULT']['COUNTRY']
 RESULTS_DIR = f'countries/{COUNTRY}/results/'
 
+
 def create_folders():
+    """
+    Function to create desired folders.
+
+    """
     os.makedirs(os.path.join(RESULTS_DIR, 'figures'), exist_ok=True)
+
 
 def create_plot(country, metric, min_population=100, under_color='b'):
     """
-        This method creates a geospatial figure depicting the predictions for a given metric within a grid.
-        
-        `min_population`: the minimum population a grid should have to be included in the scale
-        `under_color`: the color to fill squares with populations under `min_population`
-        
-        Currently, the color scale terminates 3 standard deviations above and below the mean. This is to prevent outliers
-        with extremely high relative consumptions from dominating the linear color scale.
+    This method creates a geospatial figure depicting the predictions
+    for a given metric within a grid.
+
+    `min_population`:
+
+    Currently, the color scale terminates 3 standard deviations above
+    and below the mean. This is to prevent outliers with extremely high
+    relative consumptions from dominating the linear color scale.
+
+    Parameters
+    ----------
+    country : ???
+        Country to plot.
+    metric : ???
+        Metric to plot.
+    min_population : int
+        The minimum population a grid should have to included in
+        the scale `under_color`. And squares with populations under
+        the `min_population` are filled based on the color selected
+        in `under_color`.
+    under_color : string
+        Desired color for areas which don't exceed `min_population`.
+
     """
     print(f'creating plot for {metric}')
     df_geo = gpd.read_file(f'countries/{country}/grid/grid.shp')
@@ -48,14 +70,14 @@ def create_plot(country, metric, min_population=100, under_color='b'):
     df_geo['centroid_lat'] = df_geo['centroid'].apply(lambda point: point.y)
     df_geo['centroid_lon'] = df_geo['centroid'].apply(lambda point: point.x)
     preds = pd.read_csv(os.path.join(RESULTS_DIR, f'ridge_{metric}', 'predictions.csv'))
-    
+
     if min_population is None:
         df_geo['to_ignore'] = False
     else:
         to_use = df_geo['population'] > min_population
         df_geo['to_ignore'] = True
         df_geo['to_ignore'].loc[to_use] = False
-    
+
     df_geo = merge_on_lat_lon(df_geo, preds, keys=['centroid_lat', 'centroid_lon'], how='left')
 
     geometry = df_geo['geometry']
@@ -65,10 +87,10 @@ def create_plot(country, metric, min_population=100, under_color='b'):
     vmin = coloring_guide.mean() - 3 * coloring_guide.std()
     if vmin < 0 or vmin - 0 < 0.05:
         vmin = 0
-    
+
     coloring_guide.fillna(-1, inplace=True)
     coloring_guide.loc[df_geo['to_ignore']] = -1
-    
+
     cmap = cm.get_cmap('inferno')
     cmap.set_under(under_color)
     vmax = coloring_guide.mean() + 3 * coloring_guide.std()
@@ -78,6 +100,7 @@ def create_plot(country, metric, min_population=100, under_color='b'):
             'cmap': cmap}
 
     fig, ax = plt.subplots(figsize=(10,20))
+    # ctx.add_basemap(ax)
     ax.set_aspect("equal")
     norm = colors.Normalize(vmin, vmax)
     fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
@@ -94,9 +117,13 @@ def create_plot(country, metric, min_population=100, under_color='b'):
     print(f'Saving figure to {save_dir}')
     plt.savefig(save_dir)
 
+    print('Plotting completed')
+
+
 if __name__ == '__main__':
+
     create_folders()
-    
+
     arg = '--all'
     if len(sys.argv) >= 2:
         arg = sys.argv[1]
